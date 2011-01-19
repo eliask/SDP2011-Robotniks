1ec4c4975f1e52147b3da78320c863c16bb8678d
Elias Kunnas <elias.kunnas@gmail.com>
2011-01-19 14:35:46 +0000

    Move pygame <-> CV image format conversion to utils.py

--- vision/utils.py
+++ vision/utils.py
@@ -1,4 +1,4 @@
-from opencv import cv
+from opencv import cv, highgui
 import math
 
 def BoxCenterPos(point):
@@ -13,3 +13,15 @@ def Point(x, y):
 def enum(*sequential, **named):
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
+
+def PygameToCVImage(self, pg_img):
+    tmp = tempfile.mktemp()
+    pygame.image.tostring(pg_img, tmp)
+    frame = highgui.cvLoadImage(tmp)
+    return frame
+
+def CVtoPygameImage(self, frame):
+    rgb = cv.CreateMat(frame.height, frame.width, cv.CV_8UC3)
+    cv.CvtColor(frame, rgb, cv.CV_BGR2RGB)
+    pg_img = pygame.image.frombuffer(rgb.tostring(), cv.GetSize(rgb), "RGB")
+    return pg_img
