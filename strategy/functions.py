e86af4b435fdb80f8a8bf9d959b52566dfcc8e46
Elias Kunnas <elias.kunnas@gmail.com>
2011-01-21 02:21:58 +0000

    Scaffolding for strategy -- work in progress

---
+++ strategy/functions.py
@@ -0,0 +1,95 @@
+from math import *
+from utils import *
+
+def getKickingPosition():
+    """Get kicking position and orientation.
+
+    Get the position and orientation for us to kick the ball "nicely".
+    """
+    pass
+
+def move(dest, destAngle):
+    "Move directly(?) to destination"
+    pass
+
+def constainedMove(dest, destAngle, avoid):
+    "Move while avoiding specified object(s)"
+    pass
+
+def computeGoalKicks():
+    ball = self.ents['ball']
+    targetGoal = Magic
+
+    # TODO: also add ball.left/right/etc. rays
+    pos = centerPos(ball)
+    successes = [ createRay(targetGoal, pos, radians(angle))
+                  for angle in range(360) ]
+
+    # TODO: do further processing to determine the best,
+    # "non-accidental" clusters
+    return successes
+
+
+def createRay(targetGoal, pos, angle, maxBounces=3, pos0=pos):
+    """Simulate the trajectory of an idealised ball.
+
+    targetGoal['score'](x0, x1) is a scoring function that returns a
+    utility value between -1 and 1. If we hit the right goal, we get
+    maximum utility, and minimum for the wrong one. The two arguments
+    x0 and x1 are the starting position and the tested position,
+    respectively. If we get a bit further towards the goals
+    """
+    # TODO: handle collisions with robots
+    scorefn = targetGoal['score']
+    if maxBounces == 0:
+        return scorefn(pos0, pos)
+
+    X,Y = pos
+    if -pi/2 <= angle <= pi/2:
+        dx = Pitch.right - X
+    else:
+        pitchX = X - Pitch.left
+
+    if 0 <= angle <= pi
+        dy = Pitch.top - Y
+    else:
+        dy = Y - Pitch.bottom
+
+    hitX = X + dy * tan(angle)
+    if hitX < 0:
+        imagX = hitX - dx
+        if angle == 0:
+            # this should only happen inside the goal area
+            return scorefn(pos0, (hitX, pos[1]))
+        else:
+            hitY = Pitch.top + imagX / tan(angle)
+
+    pos = hitX, hitY
+    score = scorefn(pos0, pos)
+    if abs(score) == 1
+        return score
+    else:
+        angle = reflectRay(hitX, hitY, angle)
+        createRays(targetGoal, pos, angle, maxBounces-1, pos0)
+
+
+def reflectRay(angle, hitX, hitY):
+    # Deal with small numerical inaccuracies
+    epsilon = 1e-6
+    hitX += epsilon*cos(angle)
+    hitY += epsilon*sin(angle)
+    if hitY <= Pitch.top or hitY >= Pitch.bottom:
+        return angle - pi
+    if hitX <= Pitch.left or hitX >= Pitch.right:
+        return pi - angle
+
+def computeIntercepts():
+    """Compute the possible ways to intercept a moving ball.
+
+    The idea behind our interception approach is to first move to the
+    part of the projected trajectory that's closest to us and then
+    adjusting our orientation accordingly. If we have time, approach
+    the ball by going "backwards" its trajectory.
+    """
+    pass
+
