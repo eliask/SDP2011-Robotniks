190f4dd689533b8812131a2667de9e04c0aeda97
Elias Kunnas <elias.kunnas@gmail.com>
2011-01-21 13:40:37 +0000

    Remove weird duplicate function

--- strategy/functions.py
+++ strategy/functions.py
@@ -94,18 +94,3 @@ def computeIntercepts():
     the ball by going "backwards" its trajectory.
     """
     pass
-
-def computeGoalKicks():
-    bX, bY = centerPos(self.ents['ball'])
-
-    targetGoal = Magic
-    assert targetGoal['right'] or targetGoal['left']
-
-    if targetGoal['right']:
-        angles = range(90) + range(270, 360)
-    else:
-        angles = range(90,270)
-
-    for angle in angles:
-        angle = radians(angle)
-
