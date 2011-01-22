9cafcdaa7c8f5c60762e982ff4c3c37895d77e50
Callum Lewis-Smith <callumlewis-smith@c100028.wlan.net.ed.ac.uk>
2011-01-21 16:48:08 -0800

    Added motorcommsRCX.java: program to execute commamds read from light sensor on RCX board

---
+++ robot/motorcommsRCX.java
@@ -0,0 +1,58 @@
+            import josx.platform.rcx.*;
+
+            //////////////////////////////////////
+            /**
+            * Represents a simple sample application.
+            *
+            * @author The leJOS Tutorial
+            * @version 1.0 
+            */
+            public class motorcomms {        
+
+                ////////////////////////////////////////////
+                // public methods
+                ////////////////////////////////////////////
+
+                ////////////////////////////////////////////
+                /**
+                 * main method 
+                 * @throws InterruptedException
+                 */
+                public static void main(String[] args) 
+                    throws InterruptedException {
+
+                    // message
+                    TextLCD.print("DRIVE");
+
+                    // drive forward
+		    Motor.A.setPower(7);
+		    Motor.C.setPower(7);
+
+		    Sensor.S1.setTypeAndMode(3,0x80);
+		    
+		    
+		    int loopctl = 1;
+		    boolean flag1 = false;
+		    while (loopctl > 0){
+			LCD.showNumber(Sensor.S1.readValue());
+			if(Sensor.S1.readValue() < 82){
+			    Motor.A.stop();
+			    Motor.C.stop();
+			    if (flag1 == true){
+				flag1 = false;
+			    }
+			} else if (Sensor.S1.readValue() >= 82){
+			    if (flag1 == false){
+				Motor.A.forward();
+				Motor.C.forward();
+				flag1 = true;
+			    }
+			}
+		    }		  
+
+                    // just run until RUN button is pressed again
+                    Button.RUN.waitForPressAndRelease();
+
+                } // main()
+
+            } // class SimpleSample
