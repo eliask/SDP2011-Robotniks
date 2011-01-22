3589d9ff97c04ed7e909cdc3e41f87328e8897d7
root <root@ubuntu.(none)>
2011-01-21 21:51:06 -0800

    Added MainCtrl.java : The main robot executable java file for standard operations

---
+++ robot/MainCtrl.java
@@ -0,0 +1,286 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+// Lejos imports
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+
+// Collect commands, write to screen
+public class MainCtrl {
+
+        //Defines the execution mode variable used for turning relivant test functions on/off
+        private static int execMode = 0;
+
+        //Defines the variables used for determining the position of each wheel
+        private static int steeringangle_left = 0;
+	private static int steeringangle_right = 0;
+    
+        //Defines the motors used for steering the right and left wheels
+	private static final Motor motor_left = Motor.A;
+	private static final Motor motor_right = Motor.B;
+	
+        //Defines the motor used for the kicker
+	private static final Motor motor_kick = Motor.C;
+	
+	//Defines the number of motor turns to wheel turns
+	private static final double rotConstant = 2.375;
+
+        //Defines the sensor port used to power the communication light
+        private static final SensorPort port_comlight = SensorPort.S1;
+
+        //Defines the buttons
+        private static final Button button_left = Button.LEFT;
+        private static final Button button_right = Button.RIGHT;
+        private static final Button button_enter = Button.ENTER;
+
+        // Defines variables used for the managing bluetooth connection
+	private static BTConnection connection;
+	private static DataInputStream inputStream;
+
+	public static void main(String[] args) throws InterruptedException{
+		connect();
+		collectMessage();
+	}
+	
+        //Aims to establish a conection over Bluetooth
+        public static void connect(){
+	        writeToScreen("Trying to connect", 7);
+		// Wait until connected
+		connection = Bluetooth.waitForConnection();
+		writeToScreen("Connected", 7); 
+		inputStream = connection.openDataInputStream();
+		writeToScreen("Connection Opened", 7); 
+	}
+
+        //Handles collecting the messages from the server over Bluetooth
+	private static void collectMessage() throws InterruptedException{
+		while(true){
+			try {
+				// Parse if there are any messages
+				if(inputStream.available()>0){
+				        writeToScreen("Got message",7);
+					int message = inputStream.readInt();
+					// Do specific action
+					parseMessage(message);
+					writeToScreen("Done",7);	
+				}
+			} catch (IOException e) {
+			    writeToScreen("Error",7);
+			}
+		}
+	}
+
+    //Parses integer messages
+    public static void parseMessage(int message){
+	if (message == 0){
+	    reset();
+	} else if (message == 1){
+	    drive();
+        } else if (message == 2){
+	    stop();
+	} else if (message == 3){
+	    startSpin();
+	} else if (message == 4){
+	    stopSpin();
+	} else if ((message >= 5)&&(message <=365)){
+	    setRobotDirection(message - 5);
+	}				
+    }
+
+	// Writes a message to the brick's screen on a particular line if valid
+        public static void writeToScreen(String message, int line){	
+	    if ((line >= 0)&&(line <=7)){
+	        LCD.drawString("                ", 0, line);
+		LCD.drawString(message, 0, line);
+		LCD.refresh();
+	    }
+	}
+
+        // Defines the function to provide the menu for choosing execution mode of the program
+        public static void executionMenu(){
+	   int selectedchoice = 0;
+	   int numchoices = 3;
+	   boolean enterselected = false;
+	   boolean haschanged = false;
+	   
+	   writeToScreen("Select Execution Mode",0);
+	   while (enterselected == false){
+	       //enumerates the list item when the right button is pressed
+	       if (button_right.isPressed()){
+		   if(selectedchoice < (numchoices -1)){
+		       ++selectedchoice;
+		   } else {
+		       selectedchoice = 0;
+		   }
+		   haschanged = true;
+	       }
+	       
+	       //denumerates the list item when the left button is pressed
+	       if (button_left.isPressed()){
+		   if(selectedchoice > 0){
+		       --selectedchoice;
+		   } else {
+		       selectedchoice = (numchoices - 1);
+		   }
+		   haschanged = true;
+	       }
+
+	       //deals with the enter key being pressed
+	       if (button_enter.isPressed()){
+		   enterselected = true;
+	       }
+
+	       //if the menu item has been changed this updates the screen
+	       if (haschanged == true){
+		   switch (selectedchoice){
+		   case 0:
+		       writeToScreen("1. Standard Exc.", 1);
+		       break;
+		   case 1:
+		       writeToScreen("2. Test +BT", 1);
+		       break;
+		   case 2:
+		       writeToScreen("3. Test -BT", 1);
+		       break;
+		   }
+	       }
+	       
+	       haschanged = false;
+	   }
+
+	   //executes the relevant routines based on selection
+	   switch (selectedchoice){
+	   case 0:
+	       executeStandard();
+	       break;
+	   case 1:
+	       executeTestPlusBT();
+	       break;
+	   case 2:
+	       executeTestMinBT();
+	       break;
+	   }
+       }
+	
+        // Standard execution path
+        public static void executeStandard(){
+	    connect();
+	    try{
+		collectMessage();
+	    } catch (InterruptedException e){
+		writeToScreen("Msg Col Interupt",7);
+	    }
+        }
+
+       // Test execution with Bluetooth
+       public static void executeTestPlusBT(){
+	   
+       }
+
+       // Test execution without Bluetooth
+       public static void executeTestMinBT(){
+	   
+       }
+
+	// Activate kicker
+	public static void kicker() throws InterruptedException{
+	        writeToScreen("Kick", 7);
+		motor_kick.setSpeed(800);
+		motor_kick.forward();
+		Thread.sleep(417);
+		motor_kick.stop();
+		//Button.waitForPress();
+	}
+
+       //Sets the robot's direction to the input direction in degrees
+	public static void setRobotDirection(int DirectionDEGs){
+		// For the left motor
+		if (DirectionDEGs > steeringangle_left){
+			motor_left.rotate((int)(rotConstant * (DirectionDEGs - steeringangle_left)),false);
+			steeringangle_left = DirectionDEGs;
+		} else if (DirectionDEGs < steeringangle_left){
+			motor_left.rotate((int)(-1*(steeringangle_left - DirectionDEGs)*rotConstant),false);
+			steeringangle_left = DirectionDEGs;
+		}
+		
+		// For the right motor
+		if (DirectionDEGs > steeringangle_right){
+			motor_right.rotate((int)(rotConstant * (DirectionDEGs - steeringangle_right)),false);
+			steeringangle_right = DirectionDEGs;
+		} else if (DirectionDEGs < steeringangle_right){
+			motor_right.rotate((int)(-1*(steeringangle_right - DirectionDEGs)*rotConstant),false);
+			steeringangle_right = DirectionDEGs;
+		}
+	}
+
+    //Defines the function to set the robot to spin around it's own centre
+    public static void startSpin(){
+		
+	        // Test Code to display the steering angles prior to rotation
+	           //LCD.clearDisplay();
+	           //LCD.drawString("Right Wheel Angle:",0 ,0);
+		   //LCD.drawInt(steeringangle_right,0 ,1);
+	           //LCD.drawString("Left Wheel Angle:",0 ,2);
+	           //LCD.drawInt(steeringangle_left, 0, 3);
+		   //Button.waitForPress();
+		
+		//For the left (rotate wheels to 135Deg)
+		if ((steeringangle_left % 360) > 315){
+			motor_left.rotate((int) (rotConstant * (135 + (360 - (steeringangle_left % 360)))));
+		} else if((steeringangle_left % 360) < 135){
+			motor_left.rotate((int) (rotConstant * (135 - (steeringangle_left % 360))));
+		} else if ((steeringangle_left % 360) >= 135 && ((steeringangle_left % 360) <= 315)) {
+			motor_left.rotate((int) (rotConstant * -1 * ((steeringangle_left %360) - 135)));
+		}
+		
+		steeringangle_left = 135;
+		
+		//For the right (rotate wheels to 315Deg)
+		if ((steeringangle_right % 360) > 315){
+			motor_right.rotate((int) (rotConstant * -1 *((steeringangle_right % 360)-315)));
+		} else if((steeringangle_right % 360) < 135){
+			motor_right.rotate((int) (rotConstant * -1 *(45 +( steeringangle_right % 360))));
+		} else if ((steeringangle_right % 360) >= 135 && ((steeringangle_right % 360) <= 315)) {
+			motor_right.rotate((int) (rotConstant * (180 - ((steeringangle_right % 360) - 45))));
+		}
+		
+		steeringangle_right = 315;
+
+	}
+
+    //Defines the function used to stop the robot spinning round it's own centre
+    public static void stopSpin(){
+	reset();
+    }
+
+    //Communicates with the light sensor on the RCX to start the drive motors
+    public static void drive(){
+	port_comlight.setPowerType(port_comlight.POWER_RCX9V);
+	port_comlight.activate();
+    }
+
+    //Communicates with the light sensor on the RCX to stop the drive motors
+    public static void stop(){
+	port_comlight.passivate();
+    }
+
+    //Resets both wheels to 0 deg
+    public static void reset(){
+	
+	//rotates the left wheels back to 0 deg
+	if ((steeringangle_left % 360) > 180){
+	    motor_left.rotate((int)(rotConstant * (180 - ((steeringangle_left % 360) - 180))));
+	} else if ((steeringangle_left % 360) <= 180) {
+	    motor_left.rotate((int)(rotConstant * -1 * (steeringangle_left % 360)));
+	}
+
+	//rotates the right wheels back to 0 deg
+	if ((steeringangle_right % 360) > 180){
+	    motor_right.rotate((int)(rotConstant * (180 - ((steeringangle_right % 360) - 180))));
+	} else if ((steeringangle_right % 360) <= 180){
+	    motor_right.rotate((int)(rotConstant * -1 * (steeringangle_right % 360)));
+	}
+
+    }
+}
+
