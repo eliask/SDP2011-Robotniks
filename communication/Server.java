cee50d59ae6cfb7447ac086d27ac27bda6f62527
Elias Kunnas <elias.kunnas@gmail.com>
2011-01-23 22:58:47 +0000

    Add a loopback mode for the communication server and fix the client re:newlines

--- communication/Server.java
+++ communication/Server.java
@@ -3,35 +3,41 @@ import java.net.*;
 
 public class Server {
 
-	public static void main(String[] args){
-		PCBluetooth pcb = new PCBluetooth();
-		pcb.openConnection();
-		try{
-			ServerSocket ss = new ServerSocket(6879);
-			while(true){
-				Socket socket = ss.accept();
-				InputStream is = socket.getInputStream();
-				String num = "";
-				int i;
-				char c;
-				while((i = is.read()) != -1){
-					c = (char) i;
-					if (c == '\n') {
-						int z = Integer.parseInt(num);
-						pcb.sendMessage(num);
-						System.out.println(z);
-						num = "";
-					} else {
-						num += new Character(c).toString();
-					}
-				}
-				is.close();
-			}
-		}catch(IOException e){
-			System.err.println("random io error");
-		}catch(Exception e){
-			System.err.println("something went wrong");
-		}
-	}
+    public static void main(String[] args){
+        boolean loopback = false;
+        if (args.length > 0)
+            loopback = true;
+
+        PCBluetooth pcb = new PCBluetooth();
+        if (!loopback)
+            pcb.openConnection();
+        try{
+            ServerSocket ss = new ServerSocket(6879);
+            while(true){
+                Socket socket = ss.accept();
+                InputStream is = socket.getInputStream();
+                String num = "";
+                int i;
+                char c;
+                while((i = is.read()) != -1){
+                    c = (char) i;
+                    if (c == '\n') {
+                        int z = Integer.parseInt(num);
+                        if (!loopback)
+                            pcb.sendMessage(z);
+                        System.out.println(z);
+                        num = "";
+                    } else {
+                        num += new Character(c).toString();
+                    }
+                }
+                is.close();
+            }
+        }catch(IOException e){
+            System.err.println("random io error");
+        }catch(Exception e){
+            System.err.println("something went wrong");
+        }
+    }
 
 }
--- communication/client.py
+++ communication/client.py
@@ -3,30 +3,25 @@ import socket
 class Client:
 
 	def __init__(self):
-		self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.client_socket = \
+                    socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.client_socket.connect(("localhost", 6879))
 
 	def sendMessage(self, x):
-		self.client_socket.send(str(x))
+		self.client_socket.send('%d\n' % x)
 
 	def reset(self):
 		self.sendMessage(0)
-
 	def drive(self):
 		self.sendMessage(1)
-
 	def stop(self):
 		self.sendMessage(2)
-
 	def startSpinRight(self):
 		self.sendMessage(3)
-
 	def startSpinLeft(self):
 		self.sendMessage(4)
-
 	def stopSpin(self):
 		self.sendMessage(5)
-
 	def setRobotDirection(self, angle):
                 "Turn the specified angle in radians"
 
@@ -43,10 +38,8 @@ class Client:
 
 	def kick(self):
 		self.sendMessage(366)
-
 	def spinRightShort(self):
 		self.sendMessage(367)
-
 	def spinLeftShort(self):
 		self.sendMessage(368)
 
