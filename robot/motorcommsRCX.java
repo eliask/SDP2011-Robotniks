dbf140346c2bc13c0a9283fe7605989745d4ea52
root <root@ubuntu.(none)>
2011-01-23 09:28:00 -0800

    updated motorcommsRCX to allow for sensor callibration button

--- robot/motorcommsRCX.java
+++ robot/motorcommsRCX.java
@@ -29,24 +29,29 @@
 		    Motor.C.setPower(7);
 
 		    Sensor.S1.setTypeAndMode(3,0x80);
+		    Sensor.S2.setTypeAndMode(1,0x40);
 		    
-		    
+		    int calibvalue = 81;
 		    int loopctl = 1;
 		    boolean flag1 = false;
 		    while (loopctl > 0){
 			LCD.showNumber(Sensor.S1.readValue());
-			if(Sensor.S1.readValue() < 81){
+			if(Sensor.S1.readValue() < calibvalue){
 			    Motor.A.stop();
 			    Motor.C.stop();
 			    if (flag1 == true){
 				flag1 = false;
 			    }
-			} else if (Sensor.S1.readValue() >= 81){
+			} else if (Sensor.S1.readValue() >= calibvalue){
 			    if (flag1 == false){
 				Motor.A.forward();
 				Motor.C.forward();
 				flag1 = true;
 			    }
+			}
+			
+			if(Sensor.S2.readBooleanValue()){
+			    calibvalue = Sensor.S1.readValue();
 			}
 		    }		  
 
