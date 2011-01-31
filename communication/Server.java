4e8ae7136af2c0407d06cb04c693e953cf553bd0
David Pilling <dave@darkstar.(none)>
2011-01-31 18:37:32 +0000

    accepts multiple connections.

--- communication/Server.java
+++ communication/Server.java
@@ -1,69 +1,135 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
+import java.util.ArrayList;
 
 /* Send command -1 to cleanly shutdown server, closing bluetooth connection */
 
 public class Server {
 
-    public static void main(String[] args){
-	int port = 6879;
-        boolean loopback = false;
-        if (args.length > 0)
-            loopback = true;
+	private boolean loopback = true;
+	private PCBluetooth pcb = null;
+	private int port = 6879;
+	private ServerSocket ss = null;
+	private boolean finished = false;
+	private ArrayList<Socket> sockets = new ArrayList<Socket>();
+	private ExecutorService executor = null;
 
-        PCBluetooth pcb = new PCBluetooth();
-        if (!loopback)
-            pcb.openConnection();
-        try{
-            ServerSocket ss = new ServerSocket(port);
-	    System.out.println("Server started on port " + port + ", waiting for connection.");
-serverloop:
-            while(true){
-                Socket socket = ss.accept();
-                try{
-		    System.out.println("Connection opened from " + socket.getInetAddress().toString());
-                    InputStream is = socket.getInputStream();
-                    String num = "";
-                    int i;
-                    char c;
-                    while((i = is.read()) != -1){
-                        c = (char) i;
-                        if (c == '\n') {
-                            int z = Integer.parseInt(num);
-			    if(z == -1){
-				break serverloop;
-			    }
-                            if (!loopback){
-                                try{
-                                    pcb.sendMessage(z);
-                                }catch(IOException e){
-                                    System.err.println("failed to send message. ignoring...");
-                                }
-                            }
-                            // Make the output align with PCBluetooth
-                            System.out.println("Server:      " + z);
-                            num = "";
-                        } else {
-                            num += new Character(c).toString();
-                        }
-                    }
-                    is.close();
-                }catch(IOException e){
-                    System.err.println("Client connection lost... waiting for new connection.");
-                }
+	public static void main(String[] args){
+		boolean loopback = false;
+		if (args.length > 0){
+			loopback = true;
+		}
+		new Server().startServer(loopback);
+	}
 
-            }
-        }catch(IOException e){
-            System.err.println("Could not open ServerSocket, or was interrupted.");
-        }
-	if(!loopback){
+	public void startServer(boolean loopback){
+		pcb = new PCBluetooth();
+		if (!loopback)
+			pcb.openConnection();
 		try{
-			pcb.closeConnection();
-			System.out.println("Connection to robot closed.");
+			ss = new ServerSocket(port);
+			System.out.println("Server started on port " + port + ", waiting for connection.");
+
+			serverLoop();
+
 		}catch(IOException e){
-			System.err.println("Could not close connection to robot.");
+			System.err.println("Could not open ServerSocket, or was interrupted.");
+		}
+		if(!loopback){
+			try{
+				pcb.closeConnection();
+				System.out.println("Connection to robot closed.");
+			}catch(IOException e){
+				System.err.println("Could not close connection to robot.");
+			}
+		}else{
+			System.out.println("Exiting safely in loopback mode.");
 		}
 	}
-    }
+
+	public synchronized void sendMessage(int message){
+		if (!loopback){
+			try{
+				pcb.sendMessage(message);
+			}catch(IOException e){
+				System.err.println("failed to send message. ignoring...");
+			}
+		}
+	}
+
+	public void serverLoop() throws IOException{
+		executor = Executors.newCachedThreadPool();
+		executor.execute(new ConnectionListener());
+		while(!finished){
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException e){
+				// don't care
+			}
+		}
+		ss.close();
+		for(Socket s: sockets){
+			s.close();
+		}
+		executor.shutdownNow();
+	}
+
+	class ConnectionListener implements Runnable{
+
+		public void run(){
+			try{
+				while(true){
+				Socket socket = ss.accept();
+				sockets.add(socket);
+				executor.execute(new ConnectionHandler(socket));
+			}
+			}catch(IOException e){
+				finished = true;
+				System.err.println("ServerSocket IO error");
+			}
+		}
+	}
+
+	class ConnectionHandler implements Runnable {
+
+		private Socket socket = null;
+
+		ConnectionHandler(Socket socket){
+			this.socket = socket;
+		}
+
+		public void run(){
+			try{
+				System.out.println("Connection opened from " + socket.getInetAddress().toString());
+				InputStream is = socket.getInputStream();
+				String num = "";
+				int i;
+				char c;
+				theloop:
+					while((i = is.read()) != -1){
+						c = (char) i;
+						if (c == '\n') {
+							int z = Integer.parseInt(num);
+							if(z == -1){
+								finished = true;
+								break theloop;
+							}
+							sendMessage(z);
+							// Make the output align with PCBluetooth
+							System.out.println(socket.getInetAddress().toString() + ":  " + z);
+							num = "";
+						} else {
+							num += new Character(c).toString();
+						}
+					}
+				is.close();
+				socket.close();
+				System.out.println("Connection " + socket.getInetAddress().toString() + " closed.");
+			}catch(IOException e){
+				System.err.println("Client connection lost... " + socket.getInetAddress().toString());
+			}
+		}
+	}
 
 }
