e9b4f6f0249afa1d6500b0caec6b95d63211643d
root <root@ubuntu.(none)>
2011-02-01 11:12:11 -0800

    Added a TestThreads.java to the robot codebase to test multithreading on the NXT

---
+++ robot/TestThreads.java
@@ -0,0 +1,45 @@
+// Lejos libraries
+import lejos.nxt.*;
+
+
+// Tests motors in both directions
+public class TestThreads {
+
+    public static Motor ma = Motor.A;
+    public static Motor mb = Motor.B;
+
+	public static void main(String[] args) throws InterruptedException{
+	    Thread thread1 = new RMotorAThread();
+		Thread thread2 = new RMotorBThread();
+
+		thread1.start();
+		thread2.start();
+
+		
+		Thread.sleep(3000);
+	
+	}
+}
+
+class RMotorAThread extends Thread {
+
+	Thread runner;
+	public RMotorAThread() {
+	}
+	public void run() {
+	    LCD.drawString("ma", 0, 0);
+	    TestThreads.ma.rotate(720); 
+	}
+}
+
+class RMotorBThread extends Thread {
+
+	Thread runner;
+	public RMotorBThread() {
+	}
+	public void run() {
+	    LCD.drawString("mb", 1, 0);
+	    TestThreads.mb.rotate(720);
+	}
+}
+
