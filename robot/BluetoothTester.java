f023c857d1f928eb0a917bf982ff23af243c55a8
Lynne Wallace <s0829961@sms.ed.ac.uk>
2011-02-02 18:46:55 +0000

    Robot can now send back messages

--- robot/BluetoothTester.java
+++ robot/BluetoothTester.java
@@ -1,4 +1,5 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 // Lejos imports
@@ -10,36 +11,36 @@ public class BluetoothTester {
 
 	private static BTConnection connection;
 	private static DataInputStream inputStream;
-	private static boolean keepConnOpen;
+	private static DataOutputStream outputStream;
+	private static boolean keepConnOpen = true;
 
-	public static void main(String[] args) throws InterruptedException{
-		keepConnOpen = true;
+	public static void main(String[] args) throws InterruptedException, IOException{
 		connect();
 		collectMessage();
 		close();
 	}
-	
-	public static void connect(){
+
+	public static void connect() throws IOException{
 		writeToScreen("Trying to connect");
 		// Wait until connected
 		connection = Bluetooth.waitForConnection();
 		writeToScreen("Connected"); 
 		inputStream = connection.openDataInputStream();
+		outputStream = connection.openDataOutputStream();
 		writeToScreen("Connection Opened"); 
 	}
 
 	private static void collectMessage() throws InterruptedException{
 		while(keepConnOpen){
 			try {
-				// Parse if there are any messages
-				if(inputStream.available()>0){
-					writeToScreen("Got message");
-					int message = inputStream.readInt();
-					// Do specific action
-					if (message == 0){
-						keepConnOpen = false;
-					}
-					writeToScreen("Done");	
+				int message = inputStream.readInt();
+				writeToScreen("Got message");
+				// Do specific action
+				if (message == 0){
+					keepConnOpen = false;
+				} else {
+					kick();
+					sendMessage();
 				}
 			} catch (IOException e) {
 				writeToScreen("Error");
@@ -53,16 +54,37 @@ public class BluetoothTester {
 		LCD.drawString(message, 0, 0);
 		LCD.refresh();
 	}
-	
-	// Closes connection
-	public static void close(){
-		try {
-			inputStream.close();
-			writeToScreen("Closing");
-		} catch (IOException e) {
-			writeToScreen("Could not close");
-		}
-		connection.close();
+
+	// Writes a message to the brick's screen
+	public static void writeIntToScreen(int message){
+		LCD.clearDisplay();
+		LCD.drawInt(message, 0, 0);
+		LCD.refresh();
+	}
+
+	public static void kick() throws InterruptedException, IOException{
+		writeToScreen("Kick");
+		Motor.C.setSpeed(900);
+		Motor.C.forward();
+		Thread.sleep(200);
+		Motor.C.stop();
 	}
 	
+	public static void sendMessage() throws InterruptedException, IOException{
+		int numb = outputStream.size();
+		writeIntToScreen(numb);
+		outputStream.writeInt(numb);
+		outputStream.flush();
+		Thread.sleep(200);
+		numb = outputStream.size();
+		writeIntToScreen(numb);
+	}
+
+	// Closes connection
+	public static void close() throws IOException{
+		inputStream.close();
+		outputStream.close();
+		writeToScreen("Closing");
+		connection.close();
+	}
 }
