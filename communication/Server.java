3c015bda3476acfd43cf27ebfbba9557bd922877
Elias Kunnas <elias.kunnas@gmail.com>
2011-02-03 03:27:30 +0000

    Refactor the communication server a bit

--- communication/Server.java
+++ communication/Server.java
@@ -28,42 +28,52 @@ public class Server {
 		pcb = new PCBluetooth();
 		if (!loopback)
 			pcb.openConnection();
+
 		try{
 			ss = new ServerSocket(port);
-			System.out.println("Server started on port " + port + ", waiting for connection.");
+			if (loopback)
+				System.out.println("=== Loopback mode ===");
 
+			System.out.println( "Server started on port " +
+								port + ", waiting for connection." );
 			serverLoop();
 
 		}catch(IOException e){
 			System.err.println("Could not open ServerSocket, or was interrupted.");
 		}
-		if(!loopback){
-			try{
-				pcb.closeConnection();
-				System.out.println("Connection to robot closed.");
-			}catch(IOException e){
-				System.err.println("Could not close connection to robot.");
-			}
-		}else{
+
+		if(loopback){
 			System.out.println("Exiting safely in loopback mode.");
+			return;
+		}
+
+		try{
+			pcb.closeConnection();
+			System.out.println("Connection to robot closed.");
+		}catch(IOException e){
+			System.err.println("Could not close connection to robot.");
 		}
 	}
 
 	public synchronized void sendMessage(int message){
-		if (!loopback){
-			try{
-				pcb.sendMessage(message);
-			}catch(IOException e){
-				System.err.println("failed to send message. ignoring...");
-			}
-		}
 		commandSent = true;
+		if (loopback)
+			return;
+
+		try{
+			pcb.sendMessage(message);
+		}catch(IOException e){
+			// Usually this happens when the connection dies.
+			// Should we try to reconnect?
+			System.err.println("failed to send message. ignoring...");
+		}
 	}
 
 	public void serverLoop() throws IOException{
 		executor = Executors.newCachedThreadPool();
 		executor.execute(new ConnectionListener());
 		executor.execute(new KeepAlive());
+
 		while(!finished){
 			try{
 				Thread.sleep(250);
@@ -71,6 +81,7 @@ public class Server {
 				// don't care
 			}
 		}
+
 		ss.close();
 		for(Socket s: sockets){
 			s.close();
@@ -82,32 +93,27 @@ public class Server {
 
 		public void run(){
 			while(!finished){
+				commandSent = false;
 				try{
 					Thread.sleep(5000);
 				}catch(InterruptedException e){
 					// nobody cares
 				}
 				if(!commandSent){
-					try{
-						pcb.sendMessage(0);
-					}catch(IOException e){
-						System.err.println("send message failed.");
-					}
+					sendMessage(0);
 				}
-				commandSent = false;
 			}
 		}
 	}
 
 	class ConnectionListener implements Runnable{
-
 		public void run(){
 			try{
 				while(true){
-				Socket socket = ss.accept();
-				sockets.add(socket);
-				executor.execute(new ConnectionHandler(socket));
-			}
+					Socket socket = ss.accept();
+					sockets.add(socket);
+					executor.execute(new ConnectionHandler(socket));
+				}
 			}catch(IOException e){
 				finished = true;
 				System.err.println("ServerSocket IO error");
@@ -125,33 +131,35 @@ public class Server {
 
 		public void run(){
 			try{
-				System.out.println("Connection opened from " + socket.getInetAddress().toString());
+				System.out.println( "New connection from: " +
+									socket.getInetAddress().toString() );
 				InputStream is = socket.getInputStream();
 				String num = "";
 				int i;
 				char c;
-				theloop:
-					while((i = is.read()) != -1){
-						c = (char) i;
-						if (c == '\n') {
-							int z = Integer.parseInt(num);
-							if(z == -1){
-								finished = true;
-								break theloop;
-							}
-							sendMessage(z);
-							// Make the output align with PCBluetooth
-							System.out.println(socket.getInetAddress().toString() + ":  " + z);
-							num = "";
-						} else {
-							num += new Character(c).toString();
+				while((i = is.read()) != -1){
+					c = (char) i;
+					if (c == '\n') {
+						int z = Integer.parseInt(num);
+						if(z == -1){
+							finished = true;
+							break;
 						}
+						sendMessage(z);
+						System.out.println(socket.getInetAddress().toString()
+										   + ":  " + z);
+						num = "";
+					} else { // c != '\n'
+						num += new Character(c).toString();
 					}
+				}
 				is.close();
 				socket.close();
-				System.out.println("Connection " + socket.getInetAddress().toString() + " closed.");
+				System.out.println( "Connection closed: " +
+									socket.getInetAddress().toString() );
 			}catch(IOException e){
-				System.err.println("Client connection lost... " + socket.getInetAddress().toString());
+				System.err.println( "Lost connection to client: " +
+									socket.getInetAddress().toString() );
 			}
 		}
 	}
