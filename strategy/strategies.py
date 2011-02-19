80a80e4cb0b4036ff02fa1fd8322863dc595be48
Elias Kunnas <elias.kunnas@gmail.com>
2011-02-19 18:45:50 +0000

    Hopefully now actually un-depend on google protobuf stuff

--- strategy/strategies.py
+++ strategy/strategies.py
@@ -1,9 +1,9 @@
 import main
-import main2
 import null
 import kicktest
 
 try:
+    import main2
     from mlbridge import MLBridge
 except ImportError:
     MLBridge = None
@@ -12,7 +12,7 @@ except ImportError:
 
 strategies = { 'main' : main.Main,
                'main2' : main2.Main,
-               'kicktest'   : KickTest.kicktest,
+               'kicktest'   : kicktest.KickTest,
                'null' : null.Null,
                'ML'   : MLBridge,
              }
