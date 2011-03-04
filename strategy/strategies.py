1913337c7d826fa06230ace17259ec5640d11342
Elias Kunnas <elias.kunnas@gmail.com>
2011-03-04 05:28:19 +0000

    Make the strategies loader more straightforward

--- strategy/strategies.py
+++ strategy/strategies.py
@@ -1,26 +1,25 @@
-import null
-import kicktest
+from null import Null
+from kicktest import KickTest
+from main2 import Main2
+from main3 import Main3
+from penalty_a import PenaltyA
+from penalty_d import PenaltyD
+from friendly1 import Friendly1
 
 try:
-    from main2 import Main2
-    from main3 import Main3
-    from penalty_a import PenaltyA
-    from penalty_d import PenaltyD
-    from friendly1 import Friendly1
     from mlbridge import MLBridge
 except ImportError:
     MLBridge = None
 
-"A list of strategies that can be used"
-
-strategies = { 'main2' : Main2,
-               'main3' : Main3,
-               'kicktest' : kicktest.KickTest,
+# A list of strategies that can be used:
+strategies = { 'main2'     : Main2,
+               'main3'     : Main3,
+               'kicktest'  : KickTest,
 	       'penalty_a' : PenaltyA,
 	       'penalty_d' : PenaltyD,
-               'null' : null.Null,
-               'ML'   : MLBridge,
-               'friendly1'   : Friendly1,
+               'null'      : Null,
+               'ML'        : MLBridge,
+               'friendly1' : Friendly1,
              }
 
 def list_strategies():
