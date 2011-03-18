9284c661ef21ed4bee7c34590fc34c7f3ad982a4
Elias Kunnas <elias.kunnas@gmail.com>
2011-03-18 04:36:11 +0000

    Make simulated World closer to the actual one (friction too)

--- simulator2/world.py
+++ simulator2/world.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class World(common.world.World):
 
-    Friction = 0.03
+    Friction = 0.07
 
     def __init__(self, *args):
         common.world.World.__init__(self, *args)
@@ -15,8 +15,8 @@ class World(common.world.World):
     def getRobot(self, colour):
         R = self.ents[colour]
         robot = common.world.Robot()
-        robot.pos = R.robot.body.position
-        robot.velocity = R.robot.body.velocity
+        robot.pos = np.array(R.robot.body.position)
+        robot.velocity = np.array(R.robot.body.velocity)
         robot.orientation = R.robot.body.angle % (2*pi)
         robot.ang_v = R.robot.body.angular_velocity
         robot.left_angle = R.wheel_left.body.angle % (2*pi)
@@ -26,7 +26,7 @@ class World(common.world.World):
     def getBall(self):
         B = self.ents['ball']
         ball = common.world.Ball()
-        ball.pos = B.body.position
-        ball.velocity = B.body.velocity
+        ball.pos = np.array(B.body.position)
+        ball.velocity = np.array(B.body.velocity)
         ball.ang_v = B.body.angular_velocity
         return ball
