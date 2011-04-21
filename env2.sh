af7d025cc28509c3f7f141b2a5db3c30baaa1261
Elias Kunnas <elias.kunnas@gmail.com>
2011-04-21 18:04:32 +0100

    Clean up the top-level directory a bit

--- env2.sh
+++ env2.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+# This is used for running the system on machines that have their
+# native versions of the Python packages installed. The supplied
+# packages only run on 32-bit x64 machines.
+
 . env.sh
 PYTHONPATH=
 export MPLAYER_CAPTURE=0
--- robot.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-. env.sh
-python DrRobotnik.py $1 main2
